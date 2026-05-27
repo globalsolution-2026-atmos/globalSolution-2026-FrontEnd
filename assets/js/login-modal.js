cb4a5662716ac4f8c3317c8fa3edbf4b0d32fc77
Pedro Amaro Pires <pedro.a.p.dev@gmail.com>
2026-05-27 15:42:48 -0300

    feat: adiciona ao botao acessar plataforma um modal de login, onde valida o formulario e manda para o console.

---
+++ assets/js/login-modal.js
@@ -0,0 +1,84 @@
+const modal = document.querySelector('.modal-login');
+const btnModal = document.querySelector('.btn-login');
+const closeModal = document.querySelector('.modal__close');
+
+btnModal.addEventListener('click', () => {
+    modal.showModal();
+});
+
+closeModal.addEventListener('click', () => {
+    modal.close();
+});
+
+// Valida se tem erro
+
+function toggleLoginInputError(input, hasError) {
+    input.classList.toggle("input__error", hasError);
+}
+
+//Validação formulario
+
+const name = document.querySelector('.login-name');
+const email = document.querySelector('.login-email');
+const senha = document.querySelector('.login-senha');
+const submitBtn = document.querySelector('.submit-btn')
+
+function validarLoginForm() {
+    let formularioValido = true;
+
+    const nameValue = name.value;
+    const emailValue = email.value;
+    const senhaValue = senha.value;
+
+    if (nameValue === '') {
+        toggleLoginInputError(name, true);
+        formularioValido = false;
+    } else {
+        toggleLoginInputError(name, false);
+    }
+
+    if (emailValue === '') {
+        toggleLoginInputError(email, true);
+        formularioValido = false;
+    } else if (!emailValue.includes("@")){
+        toggleLoginInputError(email, true);
+        formularioValido = false;
+    } else {
+        toggleLoginInputError(email, false);
+    }
+
+    if (senhaValue === '') {
+        toggleLoginInputError(senha, true);
+        formularioValido = false;
+    } else {
+        toggleLoginInputError(senha, false);
+    }
+
+    const ticket = {
+        nameValue,
+        emailValue,
+        senhaValue
+    };
+
+    return {
+        formularioValido,
+        ticket
+    };
+}
+
+if (submitBtn && name && email && senha && modal) {
+    submitBtn.addEventListener('click', (event)=> {
+        event.preventDefault();
+
+        const result = validarLoginForm();
+
+        if (!result.formularioValido) {
+            return;
+        }
+
+        console.log(result.ticket);
+        alert("Ticket de FAQ registrado, cheque o console.");
+        modal.close();
+
+    });
+}
