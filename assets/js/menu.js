a132b63f4236541a14ddc9be95f762119bd370ca
Pedro Amaro Pires <pedro.a.p.dev@gmail.com>
2026-06-01 09:18:15 -0300

    refactor: extrai estilos dos modulos para componente

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,21 +2,32 @@ const menuButton = document.querySelector('.menu__expand');
 const menuOpen = document.querySelector('.menu__open');
 
 menuButton.addEventListener('click', () => {
-    menuOpen.classList.toggle('active');
+    const isMenuOpen = menuOpen.classList.toggle('active');
+    menuButton.setAttribute('aria-expanded', String(isMenuOpen));
 });
 
 // Troca de paineis
-const navLinks = document.querySelectorAll('.menu-left a[data-panel]');
+const navLinks = document.querySelectorAll('.menu-left [data-panel]');
 const panels = document.querySelectorAll('.panel');
 
 navLinks.forEach(link => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
 
-        navLinks.forEach(l => l.classList.remove('active'));
+        navLinks.forEach(l => {
+            l.classList.remove('active');
+
+            if (l.hasAttribute('aria-pressed')) {
+                l.setAttribute('aria-pressed', 'false');
+            }
+        });
         panels.forEach(p => p.classList.remove('active'));
 
         link.classList.add('active');
+        if (link.hasAttribute('aria-pressed')) {
+            link.setAttribute('aria-pressed', 'true');
+        }
+
         document.getElementById('panel-' + link.dataset.panel).classList.add('active');
     })
 })
